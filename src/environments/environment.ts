b59477af4d6ec67cada532002eab9dd5e2e414ba
ProjectRyann <bryanlex2407@hotmail.com>
2025-11-07 12:54:52 -0500

    Conecté con el FireBase (agregué environments)

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyDfpr1579SkSlvsGYrTB3vn0PVU9Ez1OUE",
+    authDomain: "restaurant-6f280.firebaseapp.com",
+    projectId: "restaurant-6f280",
+    storageBucket: "restaurant-6f280.firebasestorage.app",
+    messagingSenderId: "388586203703",
+    appId: "1:388586203703:web:401bfc4948bd3a9657edb2",
+    measurementId: "G-H28TMJBPE1"
+    }
+};
